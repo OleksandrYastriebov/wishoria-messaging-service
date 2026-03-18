38b285f9e5ca4a4ea99f87132e42d8f46dcb2235
s1t1a1w1r1@gmail.com <oyastriebov@techmatesgroup.com>
2026-03-18 19:31:04 +0200

    Project init commit

---
+++ src/main/java/com/api/wishoria_messaging_service/wishoria_messaging_service/WishoriaMessagingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api.wishoria_messaging_service.wishoria_messaging_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishoriaMessagingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishoriaMessagingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/wishoria_messaging_service/wishoria_messaging_service/WishoriaMessagingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.wishoria_messaging_service.wishoria_messaging_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishoriaMessagingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
