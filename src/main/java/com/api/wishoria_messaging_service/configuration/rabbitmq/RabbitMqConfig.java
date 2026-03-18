c6cfe326d79320d331937a3ee1c5bf49c4e0899c
s1t1a1w1r1@gmail.com <oyastriebov@techmatesgroup.com>
2026-03-19 00:51:01 +0200

    Fix bugs

--- src/main/java/com/api/wishoria_messaging_service/configuration/rabbitmq/RabbitMqConfig.java
+++ src/main/java/com/api/wishoria_messaging_service/configuration/rabbitmq/RabbitMqConfig.java
@@ -2,13 +2,15 @@ package com.api.wishoria_messaging_service.configuration.rabbitmq;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
-public class RabbitMqMessagesValidationConfig implements RabbitListenerConfigurer {
+public class RabbitMqConfig implements RabbitListenerConfigurer {
 
     @Override
     public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
@@ -25,6 +27,11 @@ public class RabbitMqMessagesValidationConfig implements RabbitListenerConfigure
     @Bean
     public LocalValidatorFactoryBean amqpValidator() {
         return new LocalValidatorFactoryBean();
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
     }
 
 }
